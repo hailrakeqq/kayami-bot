28134c586c8803a7d2392f9f62e6305901800a86
psibladee <102614143+psibladee@users.noreply.github.com>
2022-05-14 11:56:17 +0300

    interactions update
    interactions update

---
+++ commands/interactions/cry.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const config = require('../../config.json')
+
+module.exports = {
+    name: "cry",
+    desc: "плакать",
+    use: "cry",
+    category: ":people_with_bunny_ears_partying: Взаимодействия",
+    aliases: [],
+
+    async execute(message, client, args) {
+        const url = `https://g.tenor.com/v1/random?q=animecry&key=${config.tenor}`
+        const response = await fetch(url)
+            .then(async response => {
+                const result = await response.json()
+                const index = Math.floor(Math.random() * result.results.length)
+                const gif = await result.results[index].media[0].gif.url
+                const embed = new MessageEmbed()
+                    .setColor(config.color)
+                    .addFields([
+                        {
+                            name: `Реакция: ${this.desc}`,
+                            value: `${message.member.user} плачет... (´｡• ᵕ •｡)`,
+                            inline: false
+                        }])
+                    .setTimestamp().setFooter({ text: `${message.member.user.tag}` })
+                    .setImage(gif)
+                message.channel.send({ embeds: [embed] });
+            })
+    },
+    admin: false
+}
---
+++ commands/interactions/wink.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const config = require('../../config.json')
+
+module.exports = {
+    name: "wink",
+    desc: "подмигнуть пользователю",
+    use: "wink",
+    category: ":people_with_bunny_ears_partying: Взаимодействия",
+    aliases: [],
+
+    async execute(message, client, args) {
+        const url = `https://g.tenor.com/v1/random?q=animewink&key=${config.tenor}`
+        const response = await fetch(url)
+            .then(async response => {
+                let mb = message.member;
+                if (message.mentions.members.size > 0) mb = message.mentions.members.first()
+                if (args[1] = '' || mb.id == message.author.id) {
+                    message.reply('Ошибка, нужно тегнуть кому вы хотите подмигнуть!!!')
+                } else {
+                    const result = await response.json()
+                    const index = Math.floor(Math.random() * result.results.length)
+                    const gif = await result.results[index].media[0].gif.url
+                    const embed = new MessageEmbed()
+                        .setColor(config.color)
+                        .addFields([
+                            {
+                                name: `Реакция: ${this.desc}`,
+                                value: `${message.member.user} подмигнул(-а) ${mb.user} (･ω<)`,
+                                inline: false
+                            }])
+                        .setTimestamp().setFooter({ text: `${message.member.user.tag}` })
+                        .setImage(gif)
+                    message.channel.send({ embeds: [embed] });
+                }
+            })
+    },
+    admin: false
+}
